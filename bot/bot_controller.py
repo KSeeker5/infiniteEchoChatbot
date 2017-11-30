9adf9bd13d42f9fab842df4bc05e2a9ee89ffb9b
KSeeker5 <alexknipper@comcast.net>
2017-11-29 22:57:35 -0500

    Update bot_controller.py

--- bot/bot_controller.py
+++ bot/bot_controller.py
@@ -178,7 +178,7 @@ class BotController:
                              ' that I have even less mercy than him. If you\'re curious, go ahead and' + 
                              ' say something to me. Who knows what will happen in your new reality. \n')
     elif used_any(BotController.COPYPASTA_WORDS) or decision_RNG > 3:
-      num_of_copypastas = randint(1, 49) #Pick a random number between 1 and 49
+      num_of_copypastas = randint(1, 50) #Pick a random number between 1 and 49
       num_of_marker = randint(1,num_of_copypastas)
       msg_to_send['text'] = ''
       copypasta_counter = 0
@@ -188,7 +188,7 @@ class BotController:
         #generated_copypasta = COPYPASTA_VALUES[copypasta_number]
         if (copypasta_counter == num_of_marker)
           msg_to_send['text'] += 'Rod5oLLEmpZ9h4ifAKnT'
-        else
+        else:
           msg_to_send['text'] += choice(BotController.COPYPASTA_VALUES) #generated_copypasta
         msg_to_send['text'] += '\n\n'
         #msg_to_send['text'] += 'a'
